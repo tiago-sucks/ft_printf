dffab007d54c7a21deb81cb916b0de846a523e93
godinho <tiagogodinho404@gmail.com>
2025-11-16 20:35:51 +0000

    so it begins...

---
+++ ft_printf.h
@@ -0,0 +1,26 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_printf.h                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: tiago <tiago@student.42.fr>                +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/11/16 19:08:38 by tiago             #+#    #+#             */
+/*   Updated: 2025/11/16 19:08:41 by tiago            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef FT_PRINTF_H
+# define FT_PRINTF_H
+
+# include <stdarg.h>
+# include <unistd.h>
+
+int	ft_printf(char const *str, ...);
+int	ft_putchar(char c);
+int	ft_putnbr(long nb);
+int	ft_putnbr_hexa(unsigned long nbr, char up);
+int	ft_putptr(void *ptr);
+int	ft_putstr(char *str);
+
+#endif
